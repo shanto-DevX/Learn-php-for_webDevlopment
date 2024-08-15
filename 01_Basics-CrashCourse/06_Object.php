586bfe126d0e013e24b0aa1f6e8f07da7e936c69
Saiful Islam Shanto <Helloshanto.DevX@gmail.com>
2024-08-15 19:29:29 +0600

    Learn Php Object

--- 01_Basics-CrashCourse/06_Object.php
+++ 01_Basics-CrashCourse/06_Object.php
@@ -1,4 +1,24 @@
 <?php
+    /* 
+        * 
+    */
 
-    
+    class phone{
+      var  $model;
+      function phoneModel($number){
+        global $model;
+        $model = $number;
+        echo "This is $model <br>";
+      }
+    }
+
+$apple = new phone;
+$apple -> phoneModel("Iphone 16");
+
+$smasung = new phone;
+$smasung -> phoneModel("smasung s24");
+
+$LG = new phone;
+$LG -> phoneModel("LG s24");
+
 ?>
