e1c410fc00b5979b970d925d9f70512ecb1b1bfd
Saiful Islam Shanto <Helloshanto.DevX@gmail.com>
2024-08-15 21:43:04 +0600

    string

---
+++ 01_Basics-CrashCourse/07_string.php
@@ -0,0 +1,17 @@
+<?php
+
+    // echo 'I Love Js & Php';
+    // echo strlen('I Love Js & Php ');
+    // echo str_word_count('I Love Js & Php ' );
+    // echo strrev('I Love Js & Php');
+    // echo strpos('I Love Js & Php', 'Js');
+
+    // echo strpos('I Love Js & Php', 'JXs');
+    // $test = strpos('I Love Js & Php', 'JXs');
+    // $test = strpos('I Love Js & Php', 'JXs');
+    // var_dump($test)
+
+    $test = strpos('I Love Js & Php', 'JXs');
+    var_dump($test)
+
+?>
